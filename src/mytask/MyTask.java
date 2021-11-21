19879361a8c6d73676eded8bdf6571b23ffccde7
jandroga <71236300+jandroga@users.noreply.github.com>
2021-11-21 21:59:50 +0100

    Table

--- src/mytask/MyTask.java
+++ src/mytask/MyTask.java
@@ -4,7 +4,12 @@
  */
 package mytask;
 
+import java.awt.Color;
+import java.awt.GridBagLayout;
 import javax.swing.JFrame;
+import mytask.Client;
+import mytask.Chef;
+import mytask.Viewer;
 
 /**
  *
@@ -12,13 +17,40 @@ import javax.swing.JFrame;
  */
 public class MyTask extends JFrame {
     
-    private Chef ch = new Chef();
+    private Client client;
+    private Chef chef;
+    private Viewer v;
+    
+    public MyTask(){
+        
+        super("Ristorante");
+        this.setLayout(new GridBagLayout());
+        this.setSize(800, 400);
+        //this.getContentPane().setBackground(Color.black);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+        
+        
+        Table t = new Table();
+        
+        client = new Client(t);
+        chef = new Chef(t);
+        v = new Viewer(t);
+        Thread tcl1 = new Thread(client);
+        tcl1.start();
 
-    /**
-     * @param args the command line arguments
-     */
+        Thread tch1 = new Thread(chef);
+        tch1.start();
+        
+        Thread tv = new Thread(v);
+        tv.start();
+
+    }
+    
+   
     public static void main(String[] args){
         // TODO code application logic here
+        new MyTask();
         
     }
     
